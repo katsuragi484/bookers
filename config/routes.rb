5e225740ae02532e619e2c8c02235e047fa9803d
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-01-10 07:01:14 +0000

    [Update]ページ体裁

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/edit' => 'books#edit'
   root to: 'homes#top'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id/edit' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
